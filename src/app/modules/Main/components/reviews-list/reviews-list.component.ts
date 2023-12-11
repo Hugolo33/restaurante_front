994d71af7faa9909621afa0a8883356d0305c99d
Marcelle Reitzel <marcellereitzel@gmail.com>
2023-12-11 10:56:00 +0100

    Creados los componentes del módulo de user

--- src/app/modules/Main/components/reviews-list/reviews-list.component.ts
+++ src/app/modules/Main/components/reviews-list/reviews-list.component.ts
@@ -14,6 +14,7 @@ export class ReviewsListComponent {
 
   async ngOnInit() {
     this.arrReviews = await this.reviewsService.getAll()
+    //Comprobado, funciona
   }
 
 }
---
+++ src/app/modules/User/components/user-aside-nav/user-aside-nav.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'user-aside-nav',
+  templateUrl: './user-aside-nav.component.html',
+  styleUrls: ['./user-aside-nav.component.css']
+})
+export class UserAsideNavComponent {
+
+}
