b9b1908c24f0864cf6c9c0824398cc29e4c35958
Marcelle Reitzel <marcellereitzel@gmail.com>
2023-12-11 09:12:40 +0100

    solución de un error

--- src/app/modules/Main/main.module.ts
+++ src/app/modules/Main/main.module.ts
@@ -5,14 +5,12 @@ import { MainRoutes } from './main.routes';
 
 import { ReactiveFormsModule } from '@angular/forms';
 
-import { LoginComponent } from './components/login/login.component';
 import { MainComponent } from './components/main/main.component';
 import { RegisterComponent } from './components/register/register.component';
 
 
 @NgModule({
     declarations: [
-        LoginComponent,
         MainComponent,
         RegisterComponent
     ],
