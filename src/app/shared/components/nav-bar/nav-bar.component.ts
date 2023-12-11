25da72dd253832b44b4468377fa16a34609a9d22
Hugo Guadalajara Lopez <guadalajarahugo@gmail.com>
2023-12-11 13:13:51 +0100

    Casi terminado el navbar principal de main

--- src/app/shared/components/nav-bar/nav-bar.component.ts
+++ src/app/shared/components/nav-bar/nav-bar.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
-  selector: 'app-nav-bar',
+  selector: 'nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.css']
 })
 export class NavBarComponent {
+
+  private router = inject(Router);
+
+  onClickReservation() {
+    Swal.fire(
+      "Please login to make a reservation"
+    )
+    this.router.navigate(['/login']);
+  }
 
 }
