b7323c8a56aea9f512ca541aa6967b5a0dc9a4eb
Hugo Guadalajara Lopez <guadalajarahugo@gmail.com>
2023-12-12 17:48:10 +0100

    Empezando los validators de los formularios de registro y de login queda por hacer lo del password y demas detalles

--- src/app/modules/Main/components/register/register.component.ts
+++ src/app/modules/Main/components/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, inject } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { UsersService } from 'src/app/core/services/users.service';
 
@@ -17,10 +17,21 @@ export class RegisterComponent {
   constructor() {
     //name, email, password, phone_number,role
     this.formulario = new FormGroup({
-      name: new FormControl(),
-      email: new FormControl(),
+      name: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(2),
+        Validators.maxLength(30),
+        Validators.pattern(/^[A-Za-z\s\xF1\xD1]+$/)
+      ]),
+      email: new FormControl(null, [
+        Validators.pattern(/^[\w.-]+@[\w.-]+.[\w.-]+$/)
+      ]),
       password: new FormControl(),
-      phone_number: new FormControl(),
+      phone_number: new FormControl(null, [
+        Validators.minLength(9),
+        Validators.maxLength(10),
+        Validators.pattern(/^([0-9])*$/)
+      ]),
     })
   }
 
@@ -34,4 +45,12 @@ export class RegisterComponent {
     }
 
   }
+
+  checkError(controlName: string, erroName: string) {
+    return this.formulario.get(controlName)?.hasError(erroName) &&
+      this.formulario.get(controlName)?.touched
+  }
+
+
+
 }
